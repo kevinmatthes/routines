66524bbaab972bf1dcf1600c32cdbf09dd6dcb62
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-01-18 15:14:33 +0100

    Prepare new instruction from existing one

---
+++ clean/clean-libraries.m
@@ -0,0 +1,96 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%% Copyright (C) 2022 Kevin Matthes
+%%
+%% This program is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation; either version 2 of the License, or
+%% (at your option) any later version.
+%%
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License along
+%% with this program; if not, write to the Free Software Foundation, Inc.,
+%% 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+%%
+%%%%
+%%
+%%  FILE
+%%      clean-objects.m
+%%
+%%  BRIEF
+%%      Remove object files.
+%%
+%%  AUTHOR
+%%      Kevin Matthes
+%%
+%%  COPYRIGHT
+%%      (C) 2022 Kevin Matthes.
+%%      This file is licensed GPL 2 as of June 1991.
+%%
+%%  DATE
+%%      2022
+%%
+%%  NOTE
+%%      See `LICENSE' for full license.
+%%      See `README.md' for project details.
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%
+%%
+%% Variables.
+%%
+%%%%
+
+% Files.
+files.self              = ' clean-objects.m ';
+
+files.types.objects.o   = '*.o';
+files.types.objects.obj = '*.obj';
+
+
+
+% Control flow.
+banner  = ['[' files.self '] '];
+
+
+
+%%%%
+%%
+%% Build steps.
+%%
+%%%%
+
+% Begin build instruction.
+disp ([banner 'Begin build instruction.']);
+
+
+
+% Check for and remove object files.
+fprintf ([banner 'Check for object files ... ']);
+
+count.o     = length (glob (files.types.objects.o));
+count.obj   = length (glob (files.types.objects.obj));
+
+if count.o + count.obj;
+    fprintf ('%d found.\n', count.o + count.obj);
+    fprintf ([banner 'Remove build artifacts ... ']);
+
+    if count.o;     delete (files.types.objects.o);     end;
+    if count.obj;   delete (files.types.objects.obj);   end;
+
+    disp ('Done.');
+else;
+    disp ('There are no object files.');
+end;
+
+
+
+% End build instruction.
+disp ([banner 'End build instruction.']);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
