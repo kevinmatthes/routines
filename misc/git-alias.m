7b2063fee333f50d86b8b9e8fc77477a5c4b76c7
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-03-12 21:30:08 +0100

    Add a Git alias

--- misc/git-alias.m
+++ misc/git-alias.m
@@ -161,6 +161,12 @@ disp ([banner '[ ' name ' ] Backup all tags.']);
 disp ([git.call name ' ' definition]);
 system ([git.call name ' ' definition]);
 
+name        = 'store';
+definition  = '"!git backup backup-drive && git save-tags backup-drive"';
+disp ([banner '[ ' name ' ] Backup all commits and tags.']);
+disp ([git.call name ' ' definition]);
+system ([git.call name ' ' definition]);
+
 name        = 'verbose';
 definition  = '"log --graph --stat --pretty=fuller --decorate --all -p"';
 disp ([banner '[ ' name ' ] Write the whole history to the terminal.']);
